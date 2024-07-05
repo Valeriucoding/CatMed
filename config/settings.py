a0f27cd912a7706988218f43134f97c467638657
Valeriucoding <valeriucelmare1@gmail.com>
2024-07-05 22:07:42 +0300

    small updates

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "catalog",
-    # third party
-    "django_tables2",
 ]
 
 MIDDLEWARE = [
@@ -57,7 +55,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
