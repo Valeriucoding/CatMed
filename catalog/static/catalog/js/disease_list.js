001f8c5ed2253559e675fddacf368594421027a4
Valeriucoding <valeriucelmare1@gmail.com>
2024-11-18 17:44:53 +0200

    fix delete modal errors

--- catalog/static/catalog/js/disease_list.js
+++ catalog/static/catalog/js/disease_list.js
@@ -5,7 +5,7 @@ function showDiseaseDeleteModal(button) {
 
     const modal = document.getElementById('disease_delete_modal');
     const messageElement = document.getElementById('delete_modal_message');
-    const confirmDeleteButton = document.getElementById('confirm_delete_button');
+    const confirmDeleteButton = document.getElementById('confirm_disease_delete_button');
     confirmDeleteButton.textContent = "Delete"
     messageElement.textContent = `Are you sure you want to delete "${diseaseName}"? This action cannot be undone.`;
 
@@ -17,7 +17,7 @@ function showDiseaseDeleteModal(button) {
 }
 
 document.body.addEventListener('htmx:afterRequest', function (event) {
-    if (event.detail.elt.id === 'confirm_delete_button' && event.detail.xhr.status === 200) {
+    if (event.detail.elt.id === 'confirm_disease_delete_button' && event.detail.xhr.status === 200) {
         const response = JSON.parse(event.detail.xhr.responseText);
         if (response.status === 'success') {
             const row = document.getElementById(`disease-${response.disease_id}-item`);
