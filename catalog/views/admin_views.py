0fdb87073bbfe928d63fc39f87bc17bff9fd5c34
Valeriucoding <valeriucelmare1@gmail.com>
2025-01-01 19:51:43 +0200

    fix autocomplete select forbidden error

--- catalog/views/admin_views.py
+++ catalog/views/admin_views.py
@@ -2,6 +2,6 @@ from django.contrib.admin.views.autocomplete import AutocompleteJsonView
 
 
 class PublicAutocompleteJsonView(AutocompleteJsonView):
-    def has_permission(self, request):
+    def has_perm(self, request):
         # Allow all anonymous AJAX requests
         return request.headers.get("x-requested-with") == "XMLHttpRequest"
