8ddbb1d8fb6fc74fcbdf0b8ed5840e45e7f8cee7
Valeriucoding <valeriucelmare1@gmail.com>
2025-02-11 18:02:35 +0200

    add PostHog user tracking

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,11 @@
-from allauth.account.views import LoginView, LogoutView, PasswordResetFromKeyView, PasswordResetView, SignupView
+import posthog
+from allauth.account.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetFromKeyView,
+    PasswordResetView,
+    SignupView,
+)
 
 
 class HTMXLoginView(LoginView):
@@ -10,6 +17,13 @@ class HTMXLoginView(LoginView):
             self.template_name = self.partial_template_name
         return super().get(request, *args, **kwargs)
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.request.user
+        posthog.identify(user.id, {"email": user.email})
+        posthog.capture(user.id, "User Logged In")
+        return response
+
 
 class HTMXSignupView(SignupView):
     template_name = "account/signup.html"
@@ -20,6 +34,13 @@ class HTMXSignupView(SignupView):
             self.template_name = self.partial_template_name
         return super().get(request, *args, **kwargs)
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = self.user
+        posthog.identify(user.id, {"email": user.email})
+        posthog.capture(user.id, "User Signed Up")
+        return response
+
 
 class HTMXLogoutView(LogoutView):
     template_name = "account/logout.html"
@@ -29,6 +50,11 @@ class HTMXLogoutView(LogoutView):
         if request.headers.get("HX-Request"):
             self.template_name = self.partial_template_name
         return super().get(request, *args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            posthog.capture(request.user.id, "User Logged Out")
+        return super().dispatch(request, *args, **kwargs)
 
 
 class HTMXForgotPasswordView(PasswordResetView):
@@ -43,11 +69,13 @@ class HTMXForgotPasswordView(PasswordResetView):
     def post(self, request, *args, **kwargs):
         if request.headers.get("HX-Request"):
             self.template_name = self.partial_template_name
-        return super().post(request, *args, **kwargs)
+        response = super().post(request, *args, **kwargs)
+        email = request.POST.get("email")
+        posthog.capture(email, "Password Reset Requested")
+        return response
 
 
 class HTMXPasswordResetConfirmView(PasswordResetFromKeyView):
-    # TODO: not working rn
     template_name = "account/password_reset_confirm.html"
     partial_template_name = "account/partials/password_reset_confirm_partial.html"
 
@@ -59,4 +87,8 @@ class HTMXPasswordResetConfirmView(PasswordResetFromKeyView):
     def post(self, request, *args, **kwargs):
         if request.headers.get("HX-Request"):
             self.template_name = self.partial_template_name
-        return super().post(request, *args, **kwargs)
+        response = super().post(request, *args, **kwargs)
+        user = request.user if request.user.is_authenticated else None
+        if user:
+            posthog.capture(user.id, "Password Reset Confirmed")
+        return response
--- docs/CatMed/Tasks.md
+++ docs/CatMed/Tasks.md
@@ -1,15 +1,24 @@
 # Urgent
+
 add user account page
-sync user with email on posthog
+add view for /accounts/password/reset/done/
+
+* /accounts/password/reset/key/4-set-password/
+* /accounts/password/reset/key/done/
+  sync user with email on posthog
 
 ### New features
 
 ### Bugs
 
 ##### Later
+
 - [] add postgresql db
+
 * add the other models
+
 - [] use tailwind cli for the production build
 
 ## Models
+
 do we need name for PresentingForm?
