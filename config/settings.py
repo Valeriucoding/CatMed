34ca1599573fa5dd3c66da2bd55b61676c2b1f2c
Valeriucoding <valeriucelmare1@gmail.com>
2025-02-22 00:17:28 +0200

    update allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=[])
 
 INTERNAL_IPS = [
     "127.0.0.1",
