b5496b1a6b61be1a37585e3f42df9fbadda7d398
Valeriucoding <valeriucelmare1@gmail.com>
2025-02-22 14:37:53 +0200

    fix allauth urls overriding accounts app urls

--- config/urls.py
+++ config/urls.py
@@ -14,16 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from debug_toolbar.toolbar import debug_toolbar_urls
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 from config.settings import DEBUG
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("accounts/", include("allauth.urls")),
     path("", include("catalog.urls")),
 ]
 
