bb77d1bc76634c4815d19da7245db448bb1b0805
Valeriucoding <valeriucelmare1@gmail.com>
2025-03-16 14:21:43 +0200

    fix autocomplete fields

--- catalog/static/catalog/js/base.js
+++ catalog/static/catalog/js/base.js
@@ -102,17 +102,17 @@ function toastManager() {
 }
 
 
-// document.addEventListener('htmx:afterSwap', function (event) {
-//     // Reinitialize form media
-//     const scripts = document.querySelectorAll('script[data-form-media]');
-//     scripts.forEach(script => {
-//         const newScript = document.createElement('script');
-//         newScript.src = script.src;
-//         newScript.setAttribute('data-form-media', '');
-//         document.body.appendChild(newScript);
-//         script.remove();
-//     });
-// });
+document.addEventListener('htmx:afterSwap', function (event) {
+    // Reinitialize form media
+    const scripts = document.querySelectorAll('script[data-form-media]');
+    scripts.forEach(script => {
+        const newScript = document.createElement('script');
+        newScript.src = script.src;
+        newScript.setAttribute('data-form-media', '');
+        document.body.appendChild(newScript);
+        script.remove();
+    });
+});
 
 // load medicine list after a medicine is deleted
 document.body.addEventListener("htmx:afterSwap", function (event) {
